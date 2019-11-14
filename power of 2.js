ace5f1cac9da209659c4187c319e18862b46edbf
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2019-11-14 16:38:59 +0530

    Create power of 2.js

---
+++ power of 2.js
@@ -0,0 +1,8 @@
+var num=parseInt(prompt("Enter the number"));
+if((Math.ceil(Math.log(num)/Math.log(2)) == (Math.floor(Math.log(num)/Math.log(2)) ))){
+    console.log("True");
+  }
+  else{
+    console.log("False");
+  }
+
