b2d1dfced1a22a75e935d2122d3b4cad36eb225b
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2019-11-14 12:31:57 +0530

    Create multiply without * operator.js

---
+++ multiply without * operator.js
@@ -0,0 +1,7 @@
+var num1=parseInt(prompt("Enter the first number"));
+var num2=parseInt(prompt("Enter the Second number"));
+var res=0;
+for(let i =1;i<=num2;i++){
+  res+=num1;
+}
+console.log(res);
