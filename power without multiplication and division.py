63814e0fb17a5b2bd87b1ce67f521077139ffc93
Surya-Narayanan0503 <52913089+Surya-Narayanan0503@users.noreply.github.com>
2019-11-14 17:15:17 +0530

    Create power without multiplication and division.py

---
+++ power without multiplication and division.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number"));
+b=int(input("Enter the second number"));
+answer=a 
+increment=a 
+for i in range(1,b): 
+     for j in range (1,a): 
+          answer+=increment 
+     increment=answer 
+print(answer) 
